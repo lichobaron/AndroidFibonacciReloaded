9e39deafd720d928307f862171e22dd91622b57c
LICHO <lichobaron@gmail.com>
2018-08-18 16:13:07 -0500

    actividades base

---
+++ app/src/main/java/co/edu/javeriana/com/fibonacci/ListPaisesActivity.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.com.fibonacci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListPaisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_paises);
+    }
+}
